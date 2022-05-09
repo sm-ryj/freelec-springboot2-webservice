c2a05a8e7315ba5aa0989b43f84483758abfa54f
YeongJoo Ryu <84553046+sm-ryj@users.noreply.github.com>
2022-05-09 14:23:15 +0900

    Chapter4 405 error

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,7 +2,9 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application { //메인 클래스 - 항상 프로젝트 최상단에 위치해야함
     public static void main(String[] args) {
